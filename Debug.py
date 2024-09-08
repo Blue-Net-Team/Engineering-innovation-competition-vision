9859a57e92d881c6ce222382e830900fe62690fe
IVEN_He <13377529851@qq.com>
2024-09-08 18:20:16 +0800

    添加调试文件，用于调整ROI位置

---
+++ Debug.py
@@ -0,0 +1,61 @@
+"""
+用于调整ROI的文件
+----
+* 封装了远程图传的用法
+* 封装了调整ROI的类，可以用鼠标来直接点击ROI的位置
+"""
+
+import cv2
+import json
+import socket
+import numpy as np
+from img_trans import ReceiveImg
+
+class ROILocater(object):
+    x0 = 0
+    y0 = 0
+    x1 = 20
+    y1 = 20
+
+    def __init__(self, cap: cv2.VideoCapture | ReceiveImg) -> None:
+        self.cap = cap
+        pass
+
+    def locate(self):
+        cv2.namedWindow("locate")
+        cv2.setMouseCallback("locate", self.mouse_callback)
+
+        while True:
+            ret, self.ori_img = self.cap.read()
+            if self.ori_img is None:
+                continue
+
+            res_img = self.draw_ROI(self.ori_img)
+
+            cv2.imshow("locate", res_img)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+    def draw_ROI(self, _frame: cv2.typing.MatLike):
+        frame = _frame.copy()
+        cv2.rectangle(frame, (self.x0, self.y0), (self.x1, self.y1), (0, 255, 0), 1)
+        return frame
+
+    def mouse_callback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.x0, self.y0 = x - 10, y - 10
+            self.x1, self.y1 = x + 10, y + 10
+
+            with open("ROI.json", "w") as f:
+                ROI = [self.x0, self.y0, self.x1, self.y1]
+                json.dump(ROI, f)
+
+
+if __name__ == "__main__":
+    cap = ReceiveImg("192.168.137.141", 8000)
+    locater = ROILocater(cap)
+
+    locater.locate()
+    cv2.destroyAllWindows()
+    print("ROI位置：", ROILocater.x0, ROILocater.y0, ROILocater.x1, ROILocater.y1)

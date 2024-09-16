e65099b975de6dc800f13a2c6345e7a24f89273e
IVEN_He <13377529851@qq.com>
2024-09-16 17:42:57 +0800

    更改网络，使得网络可以适应不同大小图片

--- model/cnn.py
+++ model/cnn.py
@@ -1,37 +1,44 @@
-import cv2
 import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
     """用神经网络识别颜色，红绿蓝白"""
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, img_size=40) -> None:
+        """
+        初始化
+        ----
+        :param img_size: 图片尺寸
+        """
+        super().__init__()
 
-        # 0号节点，将20x20的图片进行卷积
+        # 0号节点，将图片进行卷积
         self.node0 = nn.Sequential(
             nn.Conv2d(3, 64, 3, 1, 1),
             nn.ReLU(),
             nn.Conv2d(64, 64, 3, 1, 1),
             nn.ReLU(),
-            nn.AvgPool2d(2, 2),  # 输出尺寸: 64x10x10
+            nn.AvgPool2d(2, 2),  # 输出尺寸: 64x(img_size/2)x(img_size/2)
             nn.Dropout(0.25)  # 添加Dropout层
         )
 
-        # 1号节点，将10x10的图片进行卷积
+        # 1号节点，将图片进行卷积
         self.node1 = nn.Sequential(
             nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, 3, 1, 1),
             nn.ReLU(),
             nn.Conv2d(128, 128, 3, 1, 1),
             nn.ReLU(),
-            nn.AvgPool2d(2, 2),  # 输出尺寸: 128x5x5
+            nn.AvgPool2d(2, 2),  # 输出尺寸: 128x(img_size/4)x(img_size/4)
             nn.Dropout(0.25)  # 添加Dropout层
         )
+
+        # 计算全连接层的输入神经元数量
+        fc_input_size = 128 * (img_size // 4) * (img_size // 4)
 
         # 全连接层
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(128*5*5, 512),  # 调整全连接层的神经元数量
+            nn.Linear(fc_input_size, 512),  # 调整全连接层的神经元数量
             nn.ReLU(),
             nn.Dropout(0.5),  # 添加Dropout层
             nn.Linear(512, 128),

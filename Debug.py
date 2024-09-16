5e89c36d5cefce2f72b22d87cbfd89bc38e2b9e3
IVEN_He <13377529851@qq.com>
2024-09-16 23:04:37 +0800

    将json文件中的ROI四个点改成了中心点，宽高固定20px

--- Debug.py
+++ Debug.py
@@ -1,3 +1,13 @@
+#!./.venv/Scripts/python.exe
+# -*- coding: utf-8 -*-
+r"""
+* author: git config IVEN_CN && git config 13377529851@QQ.com
+* Date: 2024-09-16 17:33:27 +0800
+* LastEditTime: 2024-09-16 22:14:54 +0800
+* FilePath: \工创2025\Debug.py
+* details: 
+* Copyright (c) 2024 by IVEN, All Rights Reserved. 
+"""
 """
 用于调整ROI的文件
 ----
@@ -13,10 +23,8 @@ from img_trans import ReceiveImg
 
 
 class ROILocater(object):
-    x0 = 0
-    y0 = 0
-    x1 = 20
-    y1 = 20
+    x = 0
+    y = 0
 
     def __init__(self, cap: cv2.VideoCapture | ReceiveImg) -> None:
         self.cap = cap
@@ -24,16 +32,17 @@ class ROILocater(object):
 
     def draw_ROI(self, _frame: cv2.typing.MatLike):
         frame = _frame.copy()
-        cv2.rectangle(frame, (self.x0, self.y0), (self.x1, self.y1), (0, 255, 0), 1)
+        x0, y0 = self.x-10, self.y-10
+        x1, y1 = self.x+10, self.y+10
+        cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 255, 0), 1)
         return frame
 
     def mouse_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.x0, self.y0 = x - 10, y - 10
-            self.x1, self.y1 = x + 10, y + 10
+            self.x, self.y = x, y
 
             with open("ROI.json", "w") as f:
-                ROI = [self.x0, self.y0, self.x1, self.y1]
+                ROI = [self.x, self.y]
                 json.dump(ROI, f)
 
     def locate(self):
@@ -64,9 +73,9 @@ class ROILocater(object):
 
 
 if __name__ == "__main__":
-    cap = ReceiveImg("192.168.137.141", 8000)
+    cap = cv2.VideoCapture(0)
     locater = ROILocater(cap)
 
-    # locater.locate()      # 用于调整ROI
-    locater.show_only()     # 用于单纯接收图传图像
+    locater.locate()      # 用于调整ROI
+    # locater.show_only()     # 用于单纯接收图传图像
     cv2.destroyAllWindows()

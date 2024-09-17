3704bdd26d1f88f5dc8a7b3d371136abf0ea039f
IVEN_He <13377529851@qq.com>
2024-09-17 14:11:49 +0800

    添加检测器基类，包含了卷积锐化

---
+++ detector/Detect.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+
+class Detect:
+    """
+    检测器基类
+    ----
+    提供了卷积锐化的方法--sharpen
+    """
+    def sharpen(self, _img: cv2.typing.MatLike):
+        """
+        锐化图片
+        ----
+        :param img: 需要锐化的图片
+        """
+        # 锐化卷积核
+        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        img = _img.copy()
+        img = cv2.filter2D(img, -1, kernel)  # 锐化
+        for i in range(3):
+            img = cv2.GaussianBlur(img, (3, 3), 0)  # 高斯模糊
+        return img

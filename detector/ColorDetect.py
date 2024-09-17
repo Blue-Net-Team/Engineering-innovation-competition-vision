332e28a164d4a686d610e8cd7e2eb2713c084053
IVEN_He <13377529851@qq.com>
2024-09-17 14:11:18 +0800

    使用onnx来加载模型

--- detector/ColorDetect.py
+++ detector/ColorDetect.py
@@ -3,31 +3,23 @@
 r"""
 * author: git config IVEN_CN && git config 13377529851@QQ.com
 * Date: 2024-07-28 23:32:28 +0800
-* LastEditTime: 2024-09-16 22:20:58 +0800
+* LastEditTime: 2024-09-17 13:45:07 +0800
 * FilePath: \工创2025\detector\ColorDetect.py
 * details: 识别函数
 * Copyright (c) 2024 by IVEN, All Rights Reserved. 
 """
 import torch
-import cv2
-from model import CNN
 import torch.nn.functional as F
-
-COLOR_DICT = {
-    0:'R',
-    1:'G',
-    2:'B',
-    3:'W'
-}
+import cv2
+from detector.model import cnn
+import onnxruntime as ort
+import numpy as np
 
 class ColorDetector:
-    def __init__(self, model_path="best_model.pth"):
+    def __init__(self, model_path="best_model.onnx"):
         # 加载模型
-        self.cnn = CNN()
-        self.cnn.load_state_dict(torch.load(model_path))
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.cnn.to(self.device)
-        self.cnn.eval()
+        self.session = ort.InferenceSession(model_path, providers=['CUDAExecutionProvider' if self.device == 'cuda' else 'CPUExecutionProvider'])
 
     def detect(self, img: cv2.typing.MatLike):
         """
@@ -36,12 +28,19 @@ class ColorDetector:
         :param img: 图片
         :return: 颜色，概率
         """
-        # img = cv2.resize(img, (128, 128))
-        img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
+        img = np.expand_dims(img, axis=0).astype(np.float32)
 
-        with torch.no_grad():
-            output = self.cnn(img)
-            prediction = torch.argmax(output, dim=1)
-            probabilities = F.softmax(output, dim=1)
-        
-        return COLOR_DICT[prediction.item()], probabilities[0][prediction.item()].item()
+        inputs = {self.session.get_inputs()[0].name: img}
+        outputs = self.session.run(None, inputs)
+        output = torch.tensor(outputs[0])
+        prediction = torch.argmax(output, dim=1)
+        probabilities = F.softmax(output, dim=1)
+        return prediction, probabilities
+
+# 将模型转换为 ONNX 格式并保存
+if __name__ == "__main__":
+    cnn = cnn.CNN()
+    cnn.load_state_dict(torch.load("best_model.pth"))
+    cnn.eval()
+    dummy_input = torch.randn(1, 20, 20, 3)
+    torch.onnx.export(cnn, dummy_input, "best_model.onnx", opset_version=11)

a7f5ae7d350c3b557d1546a78b8920877baf0586
IVEN-CN <13377529851@qq.com>
2024-12-08 15:40:18 +0800

    优化代码格式，简化返回值，更新文档注释

--- Solution.py
+++ Solution.py
@@ -31,15 +31,15 @@ class Solution:
         self.color_detector = detector.ColorDetector(pth_path)
         self.line_detector = detector.LineDetector()
         self.uart = Usart(ser_port)
-        
+
         # region 读取配置文件
         try:
             with open("config.json", "r") as f:
                 config = json.load(f)
-                self.point:list[int] = config["point"]
+                self.point: list[int] = config["point"]
         except:
             self.point = [0, 0]
-        
+
         self.material_circle_detector.load_config("config.json", "material")
         self.annulus_circle_detector.load_config("config.json", "annulus")
         # endregion
@@ -49,16 +49,17 @@ class Solution:
         材料检测
         ----
         本方法会在传入的图像上画出圆形区域和颜色识别区
-        
+
         Args:
             _img (np.ndarray): 图片
         Returns:
-            list: 颜色和圆心坐标
+            str|None: 颜色
         """
-        return_lst = []  # 返回值
         img = _img.copy()
         img_sharpen = self.material_circle_detector.sharpen(img)  # 锐化
-        points, rs = self.material_circle_detector.detect_circle(img_sharpen)  # 检测圆形
+        points, rs = self.material_circle_detector.detect_circle(
+            img_sharpen
+        )  # 检测圆形
         if points is not None and rs is not None:
             for point, r in zip(points, rs):
                 # 颜色识别区顶点
@@ -84,9 +85,8 @@ class Solution:
                     (0, 255, 0),
                     1,
                 )
-                return_lst.append((color, point))
-            return return_lst
-        return [[None, None]]
+            return color
+        return None
 
     def annulus_detect(self, _img):
         """
@@ -98,38 +98,62 @@ class Solution:
             _img (np.ndarray): 图片
         Returns:
             err (None|list): 如果检测到圆环则返回None，否则返回颜色和圆心坐标与标准位置的偏差
-            
+
             `[[color, [dx,dy]],...]`
         """
         res_dict = {color: [] for color in COLOR_DIC.values()}
-        
+
         img = _img.copy()
         img_sharpen = self.annulus_circle_detector.sharpen(img)
         points, rs = self.annulus_circle_detector.detect_circle(img_sharpen)
         if points is None or rs is None:
-            return [None, None]
-        
+            return None
+
         for point, r in zip(points, rs):
             # 在原图上绘制圆环
             cv2.circle(_img, point, r, (0, 255, 0), 1)
             cv2.circle(_img, point, 1, (255, 0, 0), 1)
-            
-            color = self.detect_circle_edge_color(_img, point, r)
+
+            color, _, _ = self.detect_circle_edge_color(_img, point, r)
             res_dict[color].append(point)
-            
+
         # 将结果字典的坐标列表进行平均值计算
         for color in res_dict:
-            res_dict[color] = np.mean(res_dict[color], axis=0) if res_dict[color] else []
-                
-        errs = [
-                [color, [res_dict[color][0] - self.point[0], res_dict[color][1] - self.point[1]]] 
-                if res_dict[color] else [color, None] 
-                for color in res_dict
-            ]
+            res_dict[color] = (
+                np.mean(res_dict[color], axis=0) if res_dict[color] else []
+            )
 
-        return errs
-    
-    def detect_circle_edge_color(self, _img:cv2.typing.MatLike, point:list[int]|tuple[int], r:int):
+        # 结果列表
+        errs = [
+            (
+                [
+                    color,
+                    [
+                        res_dict[color][0] - self.point[0],
+                        res_dict[color][1] - self.point[1],
+                    ],
+                ]
+                if res_dict[color]
+                else [color, None]
+            )
+            for color in res_dict
+        ]
+
+        # 将结果列表转换成字符串，例如：
+        # "RFFFFFFFFG00421432B13450002"代表红色未检测到，绿色偏差-42,432，蓝色偏差345,2
+        # 以颜色字母开头，下一个01表示正负号，后面的数字表示偏差(补全成3位，FFF表示未检测到)
+        res = "".join(
+            [
+                f"{color}{'0' if err[1][0] < 0 else '1'}{str(err[1][0]).rjust(3, '0')}{'0' if err[1][1] < 0 else '1'}{str(err[1][1]).rjust(3, '0')}"
+                for color, err in errs
+            ]
+        )
+
+        return res
+
+    def detect_circle_edge_color(
+        self, _img: cv2.typing.MatLike, point: list[int] | tuple[int], r: int
+    ):
         """
         识别圆环的颜色
         ----
@@ -140,7 +164,7 @@ class Solution:
             point (Iterable[int]): 圆心坐标
             r (int): 圆半径
         Returns:
-            color (str): 颜色
+            tuple: 颜色、重构的图片和ROI
         """
         # region 提取圆环区域
         img = _img.copy()
@@ -148,16 +172,15 @@ class Solution:
         cv2.circle(mask, point, r, (255,), 2)
         ROI = cv2.bitwise_and(img, img, mask=mask)
         # 将ROI非零部分取出变成20*20的图片
-        polar_img = cv2.warpPolar(ROI, (2*r, r), point, r, cv2.WARP_FILL_OUTLIERS)
+        polar_img = cv2.warpPolar(ROI, (2 * r, r), point, r, cv2.WARP_FILL_OUTLIERS)
         # 将极坐标图像转换为方形图像
         square_img = cv2.resize(polar_img, (20, 20))
         # endregion
-        
+
         # 颜色识别
         color, prob = self.color_detector.detect(square_img)
 
-        return color
-        
+        return color, square_img, ROI
 
     def right_angle_detect(self, _img):
         """
@@ -169,6 +192,7 @@ class Solution:
         :return: 两直线的角度，交点坐标
         """
         angel1, angel2, cross_point = self.line_detector.find_line(_img, draw=True)
+        # TODO:更改返回值
         return angel1, angel2, cross_point
 
     def read_serial(self, head: str, tail: str):

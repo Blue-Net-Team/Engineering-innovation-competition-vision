9947fe7e07ef8d61d4f481febc4c057922e736e5
IVEN-CN <13377529851@qq.com>
2025-01-04 19:54:25 +0800

    优化Usart类，增加对端口的检查，调整read、write和clear方法以支持无效端口的处理

--- USART/communicate.py
+++ USART/communicate.py
@@ -51,52 +51,65 @@ class Usart(serial.Serial):
     * 继承了serial.Serial类，实现了使用包头包尾接受数据的方法
     * 发送数据的时候可以指定包头包尾
     """
+    port: str|None = None
 
     def __init__(self, port, baudrate=9600, timeout=None):
-        super().__init__(port=port, baudrate=baudrate, timeout=timeout)
+        self.port = port
+        if self.port:
+            super().__init__(port=port, baudrate=baudrate, timeout=timeout)
 
-    def read(self, head: str, tail: str = "\n") -> str:
-        """
-        读取数据
-        ----
-        :param head: 包头
-        :param tail: 包尾
-        :return: 数据
-        """
-        HEAD, TAIL = head.encode("ascii"), tail.encode("ascii")
-        data = b""
-        while True:
-            byte = super().read(1)
-            if not byte:
-                continue
-            data += byte
-            if data.endswith(HEAD):
-                break
+    if port:
+        def read(self, head: str, tail: str = "\n") -> str:
+            """
+            读取数据
+            ----
+            :param head: 包头
+            :param tail: 包尾
+            :return: 数据
+            """
+            HEAD, TAIL = head.encode("ascii"), tail.encode("ascii")
+            data = b""
+            while True:
+                byte = super().read(1)
+                if not byte:
+                    continue
+                data += byte
+                if data.endswith(HEAD):
+                    break
 
-        # -----读到包头-----
-        data = b""
-        while True:
-            byte = super().read(1)
-            if not byte:
-                continue
-            data += byte
-            if data.endswith(TAIL):
-                return data[: -len(TAIL)].decode("ascii")  # 返回去掉包尾的数据
+            # -----读到包头-----
+            data = b""
+            while True:
+                byte = super().read(1)
+                if not byte:
+                    continue
+                data += byte
+                if data.endswith(TAIL):
+                    return data[: -len(TAIL)].decode("ascii")  # 返回去掉包尾的数据
 
-    def write(self, data: str, head: str = "", tail: str = ""):
-        """
-        发送数据
-        ----
-        :param data: 数据
-        :param head: 包头
-        :param tail: 包尾
-        """
-        HEAD, TAIL = head.encode("ascii"), tail.encode("ascii")
-        super().write(HEAD + data.encode("ascii") + TAIL)
+        def write(self, data: str, head: str = "", tail: str = ""):
+            """
+            发送数据
+            ----
+            :param data: 数据
+            :param head: 包头
+            :param tail: 包尾
+            """
+            HEAD, TAIL = head.encode("ascii"), tail.encode("ascii")
+            super().write(HEAD + data.encode("ascii") + TAIL)
 
-    def clear(self):
-        """
-        清除缓存区
-        """
-        super().reset_input_buffer()
-        super().reset_output_buffer()
+        def clear(self):
+            """
+            清除缓存区
+            """
+            super().reset_input_buffer()
+            super().reset_output_buffer()
+    else:
+        def read(self, head: str, tail: str = "\n") -> str:
+            return ""
+
+        def write(self, data: str, head: str = "", tail: str = ""):
+            pass
+
+        def clear(self):
+            pass

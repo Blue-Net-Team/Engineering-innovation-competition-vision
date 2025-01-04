c4ed7955f1d4b3aebe82de66822b9e530e8daa58
IVEN-CN <13377529851@qq.com>
2025-01-05 05:59:49 +0800

    降低分辨率

--- utils/dataset.py
+++ utils/dataset.py
@@ -25,8 +25,8 @@ def get_all_file_paths(directory):
 class LoadCap:
     def __init__(self, _id:int=0) -> None:
         self.cap = cv2.VideoCapture(_id)
-        self.cap.set(3, 1280)
-        self.cap.set(4, 720)
+        self.cap.set(3, 640)
+        self.cap.set(4, 480)
         self.cap.set(5, 60)
         self.cap.set(6,cv2.VideoWriter.fourcc('M','J','P','G'))
 

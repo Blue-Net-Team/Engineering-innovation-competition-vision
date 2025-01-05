9ce3dc8f790486495dcdea7dab0e83588d82834c
IVEN-CN <13377529851@qq.com>
2025-01-05 17:40:06 +0800

    重构SendImg类，优化初始化逻辑，添加连接检查，改进错误处理，如果传入的ip和端口无效也不会影响运行

--- utils/ImgTrans.py
+++ utils/ImgTrans.py
@@ -30,75 +30,99 @@ import socket
 import struct
 import cv2
 import numpy as np
+from colorama import Fore, Style, init
 
+init(autoreset=True)
 
 class SendImg(object):
     """服务端视频发送"""
+    HOST: str|None=None
+    PORT: int|None=None
 
-    def __init__(self, host, port):
+    def __init__(self, host:str|None=None, port:int|None=None):
         """初始化
         ----
         Args:
             host (str): 主机IP地址
             port (int): 端口号
         """
-        self.server_socket = socket.socket()
-        self.server_socket.bind((host, port))
-        self.server_socket.listen(5)
-        self.connection = None
-        self.connect = None
-        self.stream = io.BytesIO()
+        self.HOST = host
+        self.PORT = port
 
-    def connecting(self):
-        """
-        连接客户端
-        ----
-        """
-        print("等待连接")
-        self.connection, self.client_address = self.server_socket.accept()
-        self.connect = self.connection.makefile("wb")
-        self.host_name = socket.gethostname()
-        self.host_ip = socket.gethostbyname(self.host_name)
-        print("连接成功")
+        if host and port:
+            try:
+                self.server_socket = socket.socket()
+                self.server_socket.bind((host, port))
+                self.server_socket.listen(5)
+                self.connection = None
+                self.connect = None
+                self.stream = io.BytesIO()
+            except Exception as e:
+                print(Fore.RED + "Error: ", e)
+                self.HOST = None
+                self.PORT = None
 
-    def start(self) -> None:
-        """
-        开始传输视频流
-        ----
-        运行这个函数之前，必须先运行`connecting`函数
-        """
-        print("Client Host Name:", self.host_name)
-        print("Connection from: ", self.client_address)
-        print("Streaming...")
+    if HOST and PORT:
+        def connecting(self):
+            """
+            连接客户端
+            ----
+            """
+            print("等待连接")
+            self.connection, self.client_address = self.server_socket.accept()
+            self.connect = self.connection.makefile("wb")
+            self.host_name = socket.gethostname()
+            self.host_ip = socket.gethostbyname(self.host_name)
+            print("连接成功")
 
-    def send(self, _img: cv2.typing.MatLike) -> bool:
-        """
-        发送图像数据
-        ----
-        运行这个函数之前，必须运行`start`函数
+        def start(self) -> None:
+            """
+            开始传输视频流
+            ----
+            运行这个函数之前，必须先运行`connecting`函数
+            """
+            print("Client Host Name:", self.host_name)
+            print("Connection from: ", self.client_address)
+            print("Streaming...")
 
-        Args:
-            _img (cv2.typing.MatLike): 图像数据
-        Returns:
-            res (bool): 发送是否成功
-        """
-        try:
-            img_encode = cv2.imencode(".jpg", _img, [int(cv2.IMWRITE_JPEG_QUALITY), 70])[1]
-            data_encode = np.array(img_encode)
-            self.stream.write(data_encode)
-            self.connect.write(struct.pack("<L", self.stream.tell()))
-            self.connect.flush()
-            self.stream.seek(0)
-            self.connect.write(self.stream.read())
-            self.stream.seek(0)
-            self.stream.truncate()
-            self.connect.write(struct.pack("<L", 0))
-            return True
-        except ConnectionResetError:
-            print("连接已重置")
-            self.connecting()
+        def send(self, _img: cv2.typing.MatLike) -> bool:
+            """
+            发送图像数据
+            ----
+            运行这个函数之前，必须运行`start`函数
+
+            Args:
+                _img (cv2.typing.MatLike): 图像数据
+            Returns:
+                res (bool): 发送是否成功
+            """
+            try:
+                if self.connect is None:
+                    raise ConnectionError("未连接到客户端")
+
+                img_encode = cv2.imencode(".jpg", _img, [int(cv2.IMWRITE_JPEG_QUALITY), 70])[1]
+                data_encode = np.array(img_encode)
+                self.stream.write(data_encode)
+                self.connect.write(struct.pack("<L", self.stream.tell()))
+                self.connect.flush()
+                self.stream.seek(0)
+                self.connect.write(self.stream.read())
+                self.stream.seek(0)
+                self.stream.truncate()
+                self.connect.write(struct.pack("<L", 0))
+                return True
+            except ConnectionResetError:
+                print("连接已重置")
+                self.connecting()
+                return False
+
+    else:
+        def connecting(self):
+            return None
+        def start(self):
+            return None
+        def send(self, _img: cv2.typing.MatLike):
             return False
-
 
 class ReceiveImg(object):
     """客户端接收视频流"""

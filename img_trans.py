5b756ab34f2aa160fd028cf2293f5205c72f972a
IVEN-CN <13377529851@qq.com>
2025-01-06 15:43:34 +0800

    重构图像传输逻辑，使用LoadWebCam替代SendImg，并添加图像显示功能

--- img_trans.py
+++ img_trans.py
@@ -2,10 +2,10 @@ import cv2
 from utils import LoadWebCam, SendImg
 
 if __name__ == "__main__":
-    stream = SendImg("192.168.137.141", 8000)
-    cap = cv2.VideoCapture(0)
-    stream.connecting()
-    stream.start()
-    while True:
-        img = cap.read()[1]
-        stream.send(img)
+    cap = LoadWebCam("169.254.60.115", 8000)
+    for img in cap:
+        if img is None:
+            continue
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) == ord("q"):
+            break

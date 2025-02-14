7d1dc2c0cce523f37a2c394036fcb6360e58b801
IVEN-CN <13377529851@qq.com>
2025-02-14 22:45:43 +0800

    恢复开发版本的图传，单纯用于接收图像

--- img_trans.py
+++ img_trans.py
@@ -1,20 +1,11 @@
 import cv2
-from utils import LoadWebCam, SendImg, Cap, InterpolatedCap
-from utils.ImgTrans import NeedReConnect
+from utils import LoadWebCam, SendImg
 
 if __name__ == "__main__":
-    stream = SendImg("169.254.60.115", 4444)
-    cap = InterpolatedCap()
-    while True:
-        if stream.connecting():
+    cap = LoadWebCam("192.168.139.133", 8080)
+    for img in cap:
+        if img is None:
+            continue
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) == ord("q"):
             break
-    stream.start()
-    while True:
-        try:
-            img = cap.read()[1]
-            stream.send(img)
-        except NeedReConnect:
-            while True:
-                if stream.connecting():
-                    break
-

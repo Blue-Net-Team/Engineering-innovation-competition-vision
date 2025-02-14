502679335145e4b2511198ccd00e9681465c736a
IVEN-CN <13377529851@qq.com>
2025-02-14 23:11:10 +0800

    恢复主版本的图传

--- img_trans.py
+++ img_trans.py
@@ -1,11 +1,19 @@
 import cv2
 from utils import LoadWebCam, SendImg
+from utils.ImgTrans import NeedReConnect
 
 if __name__ == "__main__":
-    cap = LoadWebCam("192.168.139.133", 8080)
-    for img in cap:
-        if img is None:
-            continue
-        cv2.imshow("img", img)
-        if cv2.waitKey(1) == ord("q"):
+    stream = SendImg("wlan0", 4444)
+    cap = cv2.VideoCapture(0)
+    while True:
+        if stream.connecting():
             break
+    stream.start()
+    while True:
+        try:
+            img = cap.read()[1]
+            stream.send(img)
+        except NeedReConnect:
+            while True:
+                if stream.connecting():
+                    break

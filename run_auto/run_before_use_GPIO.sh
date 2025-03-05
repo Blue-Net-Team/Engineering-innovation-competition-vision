e274ea4d7302bb4564240cb3d4467aff1e5a884e
IVEN-CN <13377529851@qq.com>
2025-03-05 11:56:22 +0800

    在gpio-setup.service中添加User=root配置

--- run_auto/run_before_use_GPIO.sh
+++ run_auto/run_before_use_GPIO.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
-sudo chown root:gpio /dev/gpiochip0
-sudo chmod 660 /dev/gpiochip0
-sudo chown root:gpio /dev/gpiochip1
-sudo chmod 660 /dev/gpiochip1
-sudo chown root:gpio /dev/gpiochip2
-sudo chmod 660 /dev/gpiochip2
-sudo chown root:gpio /dev/gpiochip3
-sudo chmod 660 /dev/gpiochip3
-sudo chown root:gpio /dev/gpiochip4
-sudo chmod 660 /dev/gpiochip4
+chown root:gpio /dev/gpiochip0
+chmod 660 /dev/gpiochip0
+chown root:gpio /dev/gpiochip1
+chmod 660 /dev/gpiochip1
+chown root:gpio /dev/gpiochip2
+chmod 660 /dev/gpiochip2
+chown root:gpio /dev/gpiochip3
+chmod 660 /dev/gpiochip3
+chown root:gpio /dev/gpiochip4
+chmod 660 /dev/gpiochip4

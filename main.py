7ae952d8648e47cd68d5820e8ad2cd5b423b6825
Anan-yy <3397334158@qq.com>
2025-03-24 21:32:14 +0800

    fix #6：将记录器放进utils

--- main.py
+++ main.py
@@ -44,31 +44,10 @@ import numpy as np
 
 import Solution
 from ImgTrans import SendImg, SendImgTCP, SendImgUDP
-from utils import Cap, Switch, LED, OLED_I2C, connect_to_wifi, get_CPU_temp, get_GPU_temp, printLog
+from utils import Cap, Switch, LED, OLED_I2C, recorder, connect_to_wifi, get_CPU_temp, get_GPU_temp, printLog
 from ImgTrans.ImgTrans import NeedReConnect
 
 init(autoreset=True)
-
-class Recorder:
-    """
-    记录器类，用于录制图像
-    """
-    def __init__(self):
-        self.output_path = 'run_log/' + time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()) + '.mp4'
-        self.fps = 30
-        self.frame_size = (320, 240)
-        self.writer = cv2.VideoWriter(
-            self.output_path,
-            cv2.CAP_FFMPEG,
-            self.fps,
-            self.frame_size
-        )
-
-    def record(self, frame: np.ndarray):
-        self.writer.write(frame)
-
-    def release(self):
-        self.writer.release()
 
 class MainSystem:
     """
@@ -319,7 +298,7 @@ class MainSystem:
                 # TODO:完成记录器
                 if self.deal_img_method == "record":
                     extension_txt += "\n记录器开始记录"
-                    self.recorder = Recorder()
+                    self.Recorder = recorder.Recorder()
                 else:
                     extension_txt += ""
                 self.oled.text(f"任务模式\n{extension_txt}", (1,1))
@@ -375,8 +354,8 @@ class MainSystem:
                         except Exception as e:
                             printLog(Fore.RED + f"图像处理失败:{e}" + Fore.RESET, Fore.RED)
 
-                        if self.recorder:
-                            self.recorder.record(res_img)
+                        if self.Recorder:
+                            self.Recorder.record(res_img)
 
                         if sign is None:
                             if self.switch.read_status():
@@ -449,8 +428,8 @@ class MainSystem:
 
         self.start_LED.off()
 
-        if self.recorder:
-            self.recorder.release()
+        if self.Recorder:
+            self.Recorder.release()
 
     def updateConfig(self):
         """
